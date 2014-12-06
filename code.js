268869fc631075b976a6e3b9b9ec5d7f443830d5
Panagiotis Koutsomitopoulos <PanKou.BCA@gmail.com>
2014-12-06 10:31:17 -0500

    more stuff + canvas sample

--- code.js
+++ code.js
@@ -1,17 +1,27 @@
 // JavaScript Document
 
-var volM = 20, vol=0, vols=0, sil=0, sils=0, silpvm=1, harr=0; harc=1; auto=true;
+var volM = 20, vol=0, volr=0, sil=0, silr=0, silpvm=1, harr=0, harc=1, autoc = true, autos=true;
 
 function harvest(volume){  // Takes in volume, spits out random number of silver, 
   vol = vol - volume;
   return volume*volpvm*random();
 }
 var update = function() {
-  if (auto){
+  if (autoc && vol < volM) {
+    vol = vol+volr;
+    if (vol>volM) {
+      vol = volM;
+    }
+  }
+  if (autos && vol > 0)
+  if (autos && vol > harr){
     harvest(harr);
   }
 }
 
+while(true){
+  update();
+}
 // CANVAS!
 var c = document.getElementById("cv");
 var ct = c.getContext("2d");
