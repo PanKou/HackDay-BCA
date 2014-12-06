bf49726bb5aaa6f85bcb34d4f8716341e29814b1
Panagiotis Koutsomitopoulos <PanKou.BCA@gmail.com>
2014-12-06 10:10:27 -0500

    Added auto harvester
    EASIEST THING

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
 // JavaScript Document
 
-var volM = 20, vol=0, vols=0, sil=0, sils=0, silpcm=1, harr=0; harc=1; hartf=true;
+var volM = 20, vol=0, vols=0, sil=0, sils=0, silpvm=1, harr=0; harc=1; auto=true;
 
-var harvest(volume){  // Takes in volume, spits out random number of silver
-  
+function harvest(volume){  // Takes in volume, spits out random number of silver, 
+  vol = vol - volume;
+  return volume*volpvm*random();
 }
 var update = function() {
-  if (hartf){
-  volM=volM-harr;
-  
+  if (auto){
+    harvest(harr);
   }
 }
