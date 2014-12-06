b3f9215b88c4c39fe193e8c1d8f2bfa60c43014c
Tech_Guy <support@techguysoftware.com>
2014-12-06 10:30:52 -0500

    square

--- code.js
+++ code.js
@@ -11,3 +11,23 @@ var update = function() {
     harvest(harr);
   }
 }
+
+// CANVAS!
+var c = document.getElementById("cv");
+var ct = c.getContext("2d");
+c.addEventListener('click', on_canvas_click, false);
+
+// Button code
+ct.beginPath();
+ct.fillRect(0, 0, 50, 50);
+
+
+function on_canvas_click(ev) {
+    var x = ev.clientX - Game.canvas.offsetLeft;
+    var y = ev.clientY - Game.canvas.offsetTop;
+
+    // ... x,y are the click coordinates relative to the
+    // canvas itself
+	
+	// This is for the button
+}
