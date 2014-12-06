9ec280558d6dee884229de0ae18fc9da3571f760
Panagiotis Koutsomitopoulos <PanKou.BCA@gmail.com>
2014-12-06 10:01:52 -0500

    Added some basic stuff

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 // JavaScript Document
 
-var volM = 20, vol=0, vols=0, sil=0, sils=0, silpcm=1;
+var volM = 20, vol=0, vols=0, sil=0, sils=0, silpcm=1, harr=0; harc=1; hartf=true;
+
+var harvest(volume){  // Takes in volume, spits out random number of silver
+  
+}
+var update = function() {
+  if (hartf){
+  volM=volM-harr;
+  
+  }
+}
